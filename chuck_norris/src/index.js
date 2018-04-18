73f39673885a8542d39c6fd9cf7c318098f73dec
Khayyam Jones <Khayyam.Jones@gmail.com>
2018-04-18 11:49:51 -0600

    initialized app with my name and material theme

--- chuck_norris/src/index.js
+++ chuck_norris/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const MyApp = () => (
+  <MuiThemeProvider>
+    <App />
+  </MuiThemeProvider>
+)
+
+ReactDOM.render(<MyApp />, document.getElementById('root'));
 registerServiceWorker();
